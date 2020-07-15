047fea6223f55f6a10ac2952b329c5e98380b020
CodingCCarpenter <Christineassists@aol.com>
2020-07-14 20:30:39 -0500

    complete tuples.py

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -36,12 +36,22 @@ waypoints = [
 # Add a new waypoint to the list
 # YOUR CODE HERE
 
+waypoints.append( { 
+    "lat": 37,
+    "lon": 115, 
+    "name": "area 51"
+ } )
+
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
 
 # YOUR CODE HERE
+waypoints[0]['name'] = 'not a real place'
+waypoints[0]['lon'] = -130
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for point in waypoints:
+    print(f"{point['lon']}, {point['lat']}, {point['name']")
