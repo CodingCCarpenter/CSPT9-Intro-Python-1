613da49033d2cbd9cbd0d1b8ab655f13e37c34cb
CodingCCarpenter <Christineassists@aol.com>
2020-07-14 20:38:39 -0500

    complete all but last problem of list comprehensions

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,8 +10,12 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+''' same as: 
+for i in range(1, 6):
+    y.append(i)
+'''
 
+y = [item for item in range(1, 6)]
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
