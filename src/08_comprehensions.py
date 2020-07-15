079266c1b414c3c1cf96582951339bc8610bf482
CodingCCarpenter <Christineassists@aol.com>
2020-07-14 20:42:49 -0500

    complete last problem of list comprehensions

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -11,26 +11,38 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 ''' same as: 
+y = []
 for i in range(1, 6):
     y.append(i)
 '''
 
-y = [item for item in range(1, 6)]
+y = [item for item in range(1, 6)] # could also say range(6)
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
+'''same as:
 y = []
+for i in range(1, 10):
+    y.append(i**3)
+'''
+y = [item ** 3 for item in range(1, 10)]
 
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
 
+'''same as: 
+y = []
+for i in a:
+    y.append(i.upper)
+'''
+
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [item.upper for item in a]
 
 print(y)
 
@@ -40,6 +52,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [int(item) for item in x if int(item) % 2 == 0]
 
 print(y)
--- src/14_cal.py
+++ src/14_cal.py
@@ -38,3 +38,14 @@ l = len(sys.argv)
 if l == 1:
   month = datetime.now().month
   year = datetime.now().year
+if l == 2:
+  month = int(sys.argv[1])
+  year = datetime.now().year
+if l == 3:
+  month = int(sys.argv[1])
+  year = int(sys.argv[2])
+else:
+  print('error message')
+
+cal = calendar.TextCalendar()
+cal.prmonth(year, month)
