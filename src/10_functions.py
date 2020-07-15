28d0fac67bf74ddd3beeb5e69d0d53cce0386ee2
CodingCCarpenter <Christineassists@aol.com>
2020-07-14 21:05:55 -0500

    complete 12_scopes.py exercises

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,6 +1,17 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+        '''because True is not a default return, 
+        False must also be coded in if you want an 
+        odd number to return anything other than 'none''''
+
+print(is_even(3), '**False expected') 
+print(is_even(4), '**True expected')
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
